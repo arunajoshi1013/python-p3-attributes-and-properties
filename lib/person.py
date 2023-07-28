7ecf3e9e0d9bdb3171be36a6a3d8560fb3a7cd4c
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-27 21:12:34 -0700

    attrs and props lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,23 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if ((type(name) == str) and (1 <= name.__len__() <= 25)):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
